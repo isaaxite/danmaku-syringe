5bc7db913756175a0a2e8b5aa3c9d314383916fd
isaaxite <isaacgun@outlook.com>
2026-01-31 08:51:55 +0800

    feat: 新增 Tooltip 及其副产物 Baseline

--- dev/src/Button.jsx
+++ dev/src/Button.jsx
@@ -1,9 +1,19 @@
-import { InlineButton } from "../../src/Component/Button";
-import { Page } from "./Component";
+import { InlineButton, SimpleTooltip } from "../../src/Component/Button";
+import { Block, Page } from "./Component";
 
 export default () => (
   <Page>
     <InlineButton>行内按钮-1</InlineButton>
     <InlineButton>行内按钮-2</InlineButton>
+
+    <Block>
+      <SimpleTooltip className="ml-5" placement="bottom" content="SimpleTooltip SimpleTooltip">
+        <InlineButton>Tooltip bottom</InlineButton>
+      </SimpleTooltip>
+      
+      <SimpleTooltip className="ml-30" placement="bottom-right" content="SimpleTooltip SimpleTooltip">
+        <InlineButton>Tooltip bottom-right</InlineButton>
+      </SimpleTooltip>
+    </Block>
   </Page>
 );
--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -1,4 +1,4 @@
-import { splitProps } from "solid-js";
+import { splitProps, createSignal } from "solid-js";
 
 export const InlineButton = (props) => {
   const [local, other] = splitProps(props, ['children']);
@@ -7,5 +7,75 @@ export const InlineButton = (props) => {
 
   return (
     <button {...other} className={`${btnDefClassList} mt-2 ${btnActClassList}`}>{local.children}</button>
+  );
+}
+
+const PLACEMENT_CLASSNAME = {
+  'bottom': {
+    baseline: 'bottom',
+    tooltip: 'top-1 left-1/2 -translate-x-1/2',
+    arrow: 'top-0 left-1/2 -translate-x-1/2 -mb-1'
+  },
+  'bottom-right': {
+    baseline: 'bottom',
+    tooltip: 'top-1 right-0',
+    arrow: 'top-0 right-1/4 -translate-x-1/4 -mb-1'
+  }
+};
+
+export const Baseline = (props) => {
+  const POSITION = {
+    top: 'top-0 left-0 h-0 w-full',
+    right: 'right-0 top-0 h-full w-0',
+    bottom: 'bottom-0 left-0 h-0 w-full',
+    left: 'left-0 top-0 h-full w-0',
+  };
+
+  return (
+    <div className={`overflow-visible absolute ${POSITION[props.placement]} ${props.className || ''}`}>
+      {props.children}
+    </div>
+  );
+};
+
+export function SimpleTooltip(props) {
+  const [show, setShow] = createSignal(false);
+  const placementClassName = PLACEMENT_CLASSNAME[props.placement];
+  
+  return (
+    <div class={`relative inline-block group ${props.className || ''}`}>
+      {/* Trigger */}
+      <div
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+        class="inline-block"
+      >
+        {props.children}
+      </div>
+      
+      {/* Tooltip */}
+      <Baseline className={`
+        transition-all duration-200
+        opacity-0 invisible group-hover:opacity-100 group-hover:visible
+        ${show() ? 'opacity-100 visible' : ''}  
+      `} placement={placementClassName.baseline}>
+        <div
+          class={`
+            absolute z-51 px-3 py-2 text-sm bg-slate-50
+            rounded-md shadow-lg
+            ${placementClassName.tooltip}
+          `}
+        >
+          {props.content}
+        </div>
+        {/* Arrow */}
+        <div
+          class={`
+            absolute z-50 w-2 h-2 rotate-45 bg-slate-50 shadow-inner
+            ${placementClassName.arrow}
+          `}
+        />
+      </Baseline>
+    </div>
   );
 }
